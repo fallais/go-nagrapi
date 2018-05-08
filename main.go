ae648b2fc448e8a179777212b2d2d257f7b14ba7
Alberto Bregliano <alberto.bregliano@telecomitalia.it>
2018-05-08 17:15:22 +0200

    added filter for nagios error level

--- main.go
+++ main.go
@@ -23,17 +23,21 @@ type Host struct {
 
 // Service ...
 type Service struct {
-	Name         string `json:"name,omitempty"`
-	CurrentState *int   `json:"current_state,omitempty"`
+	Name string `json:"name,omitempty"`
+	//modified to *int in order to have json correctly identify the 0 value as a real value and not nil
+	CurrentState *int `json:"current_state,omitempty"`
 }
 
 var statusFile = flag.String("s", "/data/status.dat", "Specify the location of the status file")
+
+//added the n flag to have as output only services with that level of issue
 var nagiosstate = flag.Int("n", 0, "Specify the number Nagios uses to describe the status [0, 1, 2, 3]")
 
 // GetState ...
 func GetState(w http.ResponseWriter, r *http.Request) {
 	// Parse the status file
 	sdata := nagtomaps.ParseStatus(*statusFile)
+	//needed to have a int value to check it against nagios status level
 	nagstatus := *nagiosstate
 
 	hostResponse := &Response{
@@ -47,6 +51,8 @@ func GetState(w http.ResponseWriter, r *http.Request) {
 		}
 
 		for name2, object2 := range sdata.Servicestatuslist[name] {
+
+			//if the state of the service does not match with the one in input interrupts the loop
 			currstateint, _ := strconv.Atoi(object2["current_state"])
 			if currstateint != nagstatus {
 				continue

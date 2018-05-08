1c9d83891b5895efd2acfc655edabda93b2ac8c2
Alberto Bregliano <alberto.bregliano@telecomitalia.it>
2018-05-08 16:52:04 +0200

    modified type of variable to int

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ var nagiosstate = flag.Int("n", 0, "Specify the number Nagios uses to describe t
 func GetState(w http.ResponseWriter, r *http.Request) {
 	// Parse the status file
 	sdata := nagtomaps.ParseStatus(*statusFile)
-	nagstatus := *nagiosstate
+	//nagstatus := *nagiosstate
 
 	hostResponse := &Response{
 		Hosts: nil,
@@ -53,7 +53,7 @@ func GetState(w http.ResponseWriter, r *http.Request) {
 					Name:         name2,
 					CurrentState: &currstateint,
 				}
-				if service.CurrentState == &nagstatus {
+				if service.CurrentState == nagiosstate {
 					host.Services = append(host.Services, service)
 				}
 			}
